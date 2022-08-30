e07a70d8cda76a667839070d88f98d59f8451094
kevin sarrazin <ksarrazin.dev@gmail.com>
2022-08-30 16:54:00 +0200

    fix optional to true in assignment teacher

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,6 +1,6 @@
 class Assignment < ApplicationRecord
   belongs_to :school
-  belongs_to :teacher, optional: false
+  belongs_to :teacher, optional: true
 
   validates :date, presence: true
   validates :progress, presence: true
