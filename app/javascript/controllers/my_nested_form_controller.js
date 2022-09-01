07909508cf2eb42104570b97753cb1e1c61c46bd
claude martinot <claudemartinot@orange.fr>
2022-09-01 17:48:01 +0200

    jonathan part2

--- app/javascript/controllers/my_nested_form_controller.js
+++ app/javascript/controllers/my_nested_form_controller.js
@@ -3,9 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="sort-dashboard"
 export default class extends Controller {
 
-  static targets = ["template"]
+  static targets = ["card", "list"]
 
   add() {
-    console.log(this.templateTarget)
+    console.log(this.cardTarget)
+    this.listTarget.insertAdjacentHTML("beforeend", this.cardTarget.innerHTML)
   }
 }
