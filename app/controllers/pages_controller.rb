6493ccc6b2db26aa3667409a406f772db7671026
claude martinot <claudemartinot@orange.fr>
2022-09-07 12:05:40 +0200

    dashboard apres login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
     redirect_to edit_schools_school_path(current_user.school) if current_user&.directeur?
+    redirect_to area_schools_path(current_user.area) if current_user&.gestionnaire?
   end
 end
