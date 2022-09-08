0d580915fcfe25ef476105092bebe44079a98ab3
Yann Deret <yann.deretleberre@gmail.com>
2022-09-08 17:57:44 +0200

    console log

--- app/javascript/controllers/mapanimated_controller.js
+++ app/javascript/controllers/mapanimated_controller.js
@@ -14,7 +14,6 @@ export default class extends Controller {
 
   connect() {
     mapboxgl.accessToken = this.apiKeyValue
-    console.log("Markers4")
     this.map = new mapboxgl.Map({
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
